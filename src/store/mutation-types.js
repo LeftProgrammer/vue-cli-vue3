dd2efae6fe798a755d11fb2f2da6ee8fd3ce3948
pengchun <pengchun@example.com>
2025-12-09 11:35:17 +0800

    lint: 代码格式化

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,23 +1,21 @@
-export const ACCESS_TOKEN = 'SZGC-SHIRO-TOKEN'
+export const ACCESS_TOKEN = "SZGC-SHIRO-TOKEN";
 //导航页面路由
-export const INDEX_MAIN_PAGE_PATH = '/mdgHome'
+export const INDEX_MAIN_PAGE_PATH = "/mdgHome";
 //工作中心路由
-export const INDEX_HOMEPAGE_PATH = '/homeIndex/index'
+export const INDEX_HOMEPAGE_PATH = "/homeIndex/index";
 
-
-export const LEFT_CLICK_TYPE = 'LEFT_CLICK_TYPE' //当前点击事件类型
-export const FRONT_LEFT_CLICK_TYPE = 'FRONT_LEFT_CLICK_TYPE' //上一步点击事件类型
-export const LEFT_CLICK_POSITION = 'LEFT_CLICK_POSITION' //点击事件位置
-export const LEFT_CLICK_CALLBACKS = 'LEFT_CLICK_CALLBACKS' //点击事件回调列表
-export const MOUSE_MOVE_TYPE = 'MOUSE_MOVE_TYPE' //鼠标悬浮事件类型
-export const FRONT_MOUSE_MOVE_TYPE = 'FRONT_MOUSE_MOVE_TYPE' //上一步悬浮事件类型
-export const MOUSE_MOVE_POSITION = 'MOUSE_MOVE_POSITION' //鼠标悬浮事件位置
-export const MOUSE_MOVE_CALLBACKS = 'MOUSE_MOVE_CALLBACKS' //鼠标悬浮事件回调列表
-
+export const LEFT_CLICK_TYPE = "LEFT_CLICK_TYPE"; //当前点击事件类型
+export const FRONT_LEFT_CLICK_TYPE = "FRONT_LEFT_CLICK_TYPE"; //上一步点击事件类型
+export const LEFT_CLICK_POSITION = "LEFT_CLICK_POSITION"; //点击事件位置
+export const LEFT_CLICK_CALLBACKS = "LEFT_CLICK_CALLBACKS"; //点击事件回调列表
+export const MOUSE_MOVE_TYPE = "MOUSE_MOVE_TYPE"; //鼠标悬浮事件类型
+export const FRONT_MOUSE_MOVE_TYPE = "FRONT_MOUSE_MOVE_TYPE"; //上一步悬浮事件类型
+export const MOUSE_MOVE_POSITION = "MOUSE_MOVE_POSITION"; //鼠标悬浮事件位置
+export const MOUSE_MOVE_CALLBACKS = "MOUSE_MOVE_CALLBACKS"; //鼠标悬浮事件回调列表
 
 /**
  * 定义菜单索引对象，用于标识不同类型的菜单项。
- * 
+ *
  * @constant
  * @type {Object}
  * @property {number} WEB - 表示网页类型的菜单项索引。
