625da20d479758bdc5c83351ce0dad7cc5518000
pengchun <pengchun@example.com>
2025-12-09 12:01:55 +0800

    feat: 迁移部分公共样式

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from "./router";
 import store from "./store";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import "normalize.css/normalize.css";
+import "@/styles/index.scss";
 import "./permission";
 
 const app = createApp(App);
